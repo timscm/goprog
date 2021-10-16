8116feb7ad246731850f7ea612311be220863361
Tim Zhang <timlinux@qq.com>
2021-10-17 07:54:27 +0800

    万能编程

--- ch30/deep_equal/deep_equal_test.go
+++ ch30/deep_equal/deep_equal_test.go
@@ -2,6 +2,7 @@ package deepequal_test
 
 import (
 	"errors"
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -21,15 +22,14 @@ func TestDeepEqual(t *testing.T) {
 	// c1 := Customer
 }
 
-func fillBySettings(st interface{}, settings map[string]interface{}) error {
-	if reflect.TypeOf(st).Kind() != reflect.Ptr {
-		if reflect.TypeOf(st).Elem().Kind() != reflect.Struct {
-			return errors.New("the first param should be a pointer to the struct type")
-		}
-	}
-
+func fillBySettings2(st interface{}, settings map[string]interface{}) error {
+	var (
+		field reflect.StructField
+		ok    bool
+	)
 	for k, v := range settings {
-		if field, ok := (reflect.ValueOf(st)).Elem().Type().FieldByName(k); !ok {
+		if field, ok = (reflect.ValueOf(st)).Elem().Type().FieldByName(k); !ok {
+			fmt.Println("No this field:", k)
 			continue
 		}
 		if field.Type == reflect.TypeOf(v) {
@@ -39,6 +39,43 @@ func fillBySettings(st interface{}, settings map[string]interface{}) error {
 		}
 	}
 	return nil
+}
+
+func fillBySettings(st interface{}, settings map[string]interface{}) error {
+	if reflect.TypeOf(st).Kind() != reflect.Ptr {
+		if reflect.TypeOf(st).Elem().Kind() != reflect.Struct {
+			return errors.New("the first param should be a pointer to the struct type")
+		}
+	}
+
+	var (
+		field reflect.StructField
+		ok    bool
+	)
+
+	for k, v := range settings {
+		if field, ok = (reflect.ValueOf(st)).Elem().Type().FieldByName(k); !ok {
+			continue
+		}
+		if field.Type == reflect.TypeOf(v) {
+			vstr := reflect.ValueOf(st)
+			vstr = vstr.Elem()
+			vstr.FieldByName(k).Set(reflect.ValueOf(v))
+		}
+	}
+	return nil
+}
+
+type Employee struct {
+	EmployeeID string
+	Name       string `format:"normal"`
+	Age        int
+}
+
+type Customer struct {
+	CookieID string
+	Name     string
+	// Age      int
 }
 
 func TestFillNameAndAge(t *testing.T) {
