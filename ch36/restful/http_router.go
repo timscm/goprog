8f9648c1a5c554351da9b89c8e013d24cb39d27f
Tim Zhang <timlinux@qq.com>
2021-10-18 23:15:47 +0800

    restful http

---
+++ ch36/restful/http_router.go
@@ -0,0 +1,31 @@
+/*
+更好的Router
+go get github.com/julienschmidt/httprouter
+
+面向资源的架构：Resource Oriented Architecture(ROA)
+*/
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintf(w, "Welcome!\n")
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
